2988a15656a58fdff1de0e3fb81fa860f969d6fe
Jhaines1988 <josephthaines@gmail.com>
2022-04-19 16:52:30 -0600

    basic stubbing of routes

---
+++ src/components/navigation.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import * as styles from './navigation.module.css';
+
+const Navigation = () => (
+  <nav role='navigation' className={styles.container}></nav>
+);
+
+export default Navigation;
