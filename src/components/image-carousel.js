dd97606fe28f99d0a674667c1b48025316ed6245
Jhaines1988 <josephthaines@gmail.com>
2022-05-04 17:25:37 -0600

    playing with image formats for about hero and carousel

--- src/components/image-carousel.js
+++ src/components/image-carousel.js
@@ -1,18 +1,20 @@
 import React, { useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { wrap } from 'popmotion';
+import { library, icon } from '@fortawesome/fontawesome-svg-core';
 import * as styles from './image-carousel.module.css';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import '../utils/font-awesome';
 const variants = {
   enter: (direction) => {
     return {
-      x: direction > 0 ? 1000 : -1000,
+      // x: direction > 0 ? 1000 : -1000,
       opacity: 0,
     };
   },
   center: {
     zIndex: 1,
-    x: 0,
+    // x: 0,
     opacity: 1,
   },
   // exit: (direction) => {
@@ -28,7 +30,11 @@ const swipeConfidenceThreshold = 10000;
 const swipePower = (offset, velocity) => {
   return Math.abs(offset) * velocity;
 };
-
+const chevronRight = icon({
+  prefix: 'fas',
+  iconName: 'chevron-right',
+});
+const chevronLeft = icon({ prefix: 'fas', iconName: 'chevron-left' });
 const ImageCarousel = ({ cloudinaryImages }) => {
   const [[page, direction], setPage] = useState([0, 0]);
   const imageIndex = wrap(0, cloudinaryImages.length, page);
@@ -40,10 +46,15 @@ const ImageCarousel = ({ cloudinaryImages }) => {
   return (
     <>
       <AnimatePresence initial={false} custom={direction}>
-        <div className={styles.animatedDiv}>
-          <div className={styles.imageWrapper}>
+        <motion.div className={styles.animatedDiv}>
+          <motion.div className={styles.imageWrapper}>
             <button className={styles.prev} onClick={() => paginate(-1)}>
-              {'‣'}
+              <FontAwesomeIcon
+                title='previous image in carousel'
+                icon={chevronLeft}
+                size='2xl'
+                fixedWidth
+              />
             </button>
             <motion.img
               className={styles.gatsbyImg}
@@ -51,12 +62,12 @@ const ImageCarousel = ({ cloudinaryImages }) => {
               src={cloudinaryImages[imageIndex].secure_url}
               custom={direction}
               variants={variants}
-              // initial='enter'
-              // animate='center'
+              initial='enter'
+              animate='center'
               // exit='exit'
               transition={{
                 x: { type: 'spring', stiffness: 300, damping: 30 },
-                opacity: { duration: 0.6 },
+                opacity: { duration: 0.9 },
               }}
               drag='x'
               dragConstraints={{ left: 0, right: 0 }}
@@ -72,10 +83,15 @@ const ImageCarousel = ({ cloudinaryImages }) => {
               }}
             />
             <button className={styles.next} onClick={() => paginate(1)}>
-              {'‣'}
+              <FontAwesomeIcon
+                title='next image in carousel'
+                icon={chevronRight}
+                size='2xl'
+                fixedWidth
+              />
             </button>
-          </div>
-        </div>
+          </motion.div>
+        </motion.div>
       </AnimatePresence>
     </>
   );
---
+++ src/utils/font-awesome.js
@@ -0,0 +1,5 @@
+import { library, icon } from '@fortawesome/fontawesome-svg-core';
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
+library.add(faChevronRight, faChevronLeft, faCamera);
