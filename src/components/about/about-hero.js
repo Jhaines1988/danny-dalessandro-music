c45361634d365ea737fce75dfc3b8fc75e61577b
Jhaines1988 <josephthaines@gmail.com>
2022-05-05 11:00:07 -0600

    reduced text sizing on contentful blog to make room for larger image. applied styles to the inspired text to match

--- src/components/about/about-hero.js
+++ src/components/about/about-hero.js
@@ -2,7 +2,7 @@ import { GatsbyImage } from 'gatsby-plugin-image';
 import React from 'react';
 import * as styles from './about-hero.module.css';
 import { graphql, useStaticQuery } from 'gatsby';
-import GbiBridged from '../gbi-bridged';
+
 const AboutHero = ({ hero }) => {
   const data = useStaticQuery(graphql`
     {
@@ -15,13 +15,13 @@ const AboutHero = ({ hero }) => {
       }
     }
   `);
-
+  // Hero = Cloudinary data
   const image = data.contentfulMusician.heroImage.gatsbyHero.gatsbyImageData;
   return (
-    <div className={styles.imageContainer}>
-      <img src={hero} className={styles.heroImage} />
-      {/* <GatsbyImage image={image} className={styles.heroImage} /> */}
-    </div>
+    // <div className={styles.imageContainer}>
+    // <img src={hero} className={styles.heroImage} />
+    <GatsbyImage image={image} className={styles.heroImage} />
+    // </div>
   );
 };
 {
