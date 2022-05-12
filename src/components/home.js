b1870fa8c7b5330628bcacf4d43247905d524432
Jhaines1988 <josephthaines@gmail.com>
2022-05-12 09:00:47 -0600

    added gatsby image back to home page

--- src/components/home.js
+++ src/components/home.js
@@ -3,45 +3,44 @@ import * as styles from './home.module.css';
 
 import { navigate } from 'gatsby';
 import { graphql, useStaticQuery } from 'gatsby';
+import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 
 import VideoBanner from './videobanner';
 const Home = () => {
-  // const data = useStaticQuery(graphql`
-  //   {
-  //     contentfulHeroImages(isHomePageHero: { eq: true }) {
-  //       id
-  //       hero {
-  //         gatsbyImageData(layout: FULL_WIDTH)
-  //       }
-  //     }
-  //   }
-  // `);
   const data = useStaticQuery(graphql`
     {
       contentfulHeroImages(isHomePageHero: { eq: true }) {
         id
-        childContentfulHeroImagesHeroJsonNode {
-          secure_url
+        gatsbyHero {
+          gatsbyImageData(
+            layout: CONSTRAINED
+            width: 2500
+            cropFocus: TOP
+            placeholder: TRACED_SVG
+          )
         }
       }
     }
   `);
+
+  const image = getImage(data.contentfulHeroImages.gatsbyHero);
+  // const data = useStaticQuery(graphql`
+  //   {
+  //     contentfulHeroImages(isHomePageHero: { eq: true }) {
+  //       id
+  //       childContentfulHeroImagesHeroJsonNode {
+  //         secure_url
+  //       }
+  //     }
+  //   }
+  // `);
   // const image = getImage(data.contentfulHeroImages.hero);
 
   return (
     <div className={styles.outerContainer}>
       <VideoBanner />
-      {/* <div
-          style={{
-            backgroundImage: `url(${data.contentfulHeroImages.childContentfulHeroImagesHeroJsonNode.secure_url})`,
-            backgroundPosition: 'center',
-            objectFit: 'cover',
-            objectPosition: 'center',
-          }}
-        style={{
-          display: 'grid',
-        }}> */}
-      {/* <GatsbyImage image={image} alt='' className={styles.imageWrapper} /> */}
+
+      <GatsbyImage image={image} alt='' className={styles.imageWrapper} />
       {/* <img
           className={styles.imageWrapper}
           src={
@@ -64,13 +63,13 @@ const Home = () => {
           <li className={styles.listItem}>Studio Musician</li>
           <li className={styles.listItem}>Arranger</li>
         </ul>
-        <button
+        {/* <button
           className={styles.contactButton}
           onClick={() => {
             navigate('/contact');
           }}>
           Contact
-        </button>
+        </button> */}
       </div>
     </div>
     // </div>
