5d748128d2f4aad7517d7f844e0ed3aded5ad81b
Jhaines1988 <josephthaines@gmail.com>
2022-05-18 09:12:34 -0600

    listen page layout restyled, still needs some general styling for background

--- src/pages/listen.js
+++ src/pages/listen.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { graphql } from 'gatsby';
-// import VideoBanner from '../components/listen/videobanner';
 import ListenContainer from '../components/listen/listen-container';
 import Layout from '../components/layout';
-// import GbiBridged from '../components/background/gbi-bridged';
+
 const ListenPage = ({ data }) => {
   const bands = data.allContentfulBand.edges;
-  const image = data.contentfulBackgroundImages.backgroundImage;
+
   return (
-    // <GbiBridged image={image}>
     <Layout>
-      {/* <VideoBanner /> */}
       <ListenContainer bands={bands} />
     </Layout>
-    // </GbiBridged>
   );
 };
 
@@ -37,11 +33,6 @@ export const query = graphql`
             }
           }
         }
-      }
-    }
-    contentfulBackgroundImages(collectionName: { eq: "Listen-Background" }) {
-      backgroundImage {
-        gatsbyImageData(layout: FULL_WIDTH, placeholder: TRACED_SVG)
       }
     }
   }
