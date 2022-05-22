9c8ed771ab84cdacb76257cdb019361cb8c33544
Jhaines1988 <josephthaines@gmail.com>
2022-05-22 12:08:50 -0600

    home page and nav bar touch up

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import * as styles from './navigation.module.css';
-import icon from '../images/icon.png';
+import icon from '../images/Logo White.png';
 
 const Navigation = () => {
   return (
