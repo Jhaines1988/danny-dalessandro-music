d1504fe664f3388095d3d741f18170e03979f94d
Jhaines1988 <josephthaines@gmail.com>
2022-05-23 12:48:12 -0600

    styled events to specifications, needs design review

--- src/components/events/event-extras.js
+++ src/components/events/event-extras.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import * as styles from './event-extras.module.css';
+import NavButton from './internal-nav-buttons';
 const EventsExtras = ({ eventExtrasText }) => {
   return (
     <div className={styles.eventsExtrasContainer}>
@@ -10,6 +11,7 @@ const EventsExtras = ({ eventExtrasText }) => {
           __html: eventExtrasText,
         }}
       />
+      <NavButton text='Contact' page='/contact' />
     </div>
   );
 };
